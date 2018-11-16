41416d3976d35519d28e9813eaa892a18318d12b
Tony Yang <tianyuyang658@yahoo.com>
2018-11-16 01:04:03 -0800

    Adding the base cache class with unit test and coverage

--- py_cached/py_cached/core/cache.py
+++ py_cached/py_cached/core/cache.py
@@ -4,3 +4,10 @@ logger = logging.getLogger(__name__)
 class Cache:
     def __init__(self):
         logger.info('Create a Cache object')
+        self.cache = {}
+
+    def set(self, k, v):
+        self.cache[k] = v
+
+    def get(self, k):
+        return self.cache[k]
--- py_cached/tests/unit/test_cache.py
+++ py_cached/tests/unit/test_cache.py
@@ -1,10 +1,17 @@
 import unittest
 
+from py_cached.core.cache import Cache
+
 import py_cached.settings_tests
+import logging
+logger = logging.getLogger(__name__)
 
 class TestCache(unittest.TestCase):
     def setUp(self):
-        pass
+        self.c = Cache()
 
-    def test_success(self):
-        self.assertTrue(True)
+    def test_set_key(self):
+        self.c.set('test-key', 15)
+        output = self.c.get('test-key')
+        expected = 15
+        self.assertEqual(output, expected, 'ERROR: calling get on the key returns incorrect value')
